ff21c1665ded8c3701668da1adcc733c96dfbdec
ryandkuster <rkuster@utk.edu>
2025-05-16 15:45:15 -0400

    updated tests

--- tests/run_truvari.sh
+++ tests/run_truvari.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 singularity exec docker://quay.io/biocontainers/truvari:5.3.0--pyhdfd78af_0 \
-    truvari anno svinfo -o truvari.vcf lol.vcf
+    truvari anno svinfo -o truvari.vcf test.vcf
